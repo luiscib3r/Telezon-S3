1da9054eab9ba4c79fdfadd0f97348cf2f3dd95d
Luis Correa <correaleyval@gmail.com>
2021-03-19 18:55:54 -0400

    Use telegram account to send and download files

--- app/storage/__init__.py
+++ app/storage/__init__.py
@@ -1,4 +1,4 @@
 from app.storage.storage import Storage
 from app.storage.telegram import TelegramAccountStorage, TelegramBotStorage
 
-storage: Storage = TelegramAccountStorage
+storage: Storage = TelegramAccountStorage()
